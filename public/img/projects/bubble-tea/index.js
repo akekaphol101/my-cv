4c5ce6a1aa951a549d19b492e1ce65bfab9da719
akekaphol101 <akekaphol101@gmail.com>
2025-10-17 00:30:38 +0700

    add image.

---
+++ public/img/projects/bubble-tea/index.js
@@ -0,0 +1,14 @@
+import BubbleTea1 from "./success1.jpg";
+import BubbleTea2 from "./b1.jpg";
+import BubbleTea3 from "./UIFinal.png";
+import BubbleTea4 from "./b2.jpg";
+
+
+export {BubbleTea1, BubbleTea2, BubbleTea3, BubbleTea4};
+export default  {
+    BubbleTea1,
+    BubbleTea2,
+    BubbleTea3,
+    BubbleTea4
+
+}
---
+++ public/img/projects/image-analytic/index.js
@@ -0,0 +1,21 @@
+import iAnalytic1 from "./1.jpg";
+import iAnalytic2 from "./2.jpg";
+import iAnalytic3 from "./3.jpg";
+import iAnalytic4 from "./4.jpg";
+import iAnalytic5 from "./5.jpg";
+
+export { 
+    iAnalytic1,
+    iAnalytic2,
+    iAnalytic3,
+    iAnalytic4,
+    iAnalytic5
+};
+export default {
+    iAnalytic1,
+    iAnalytic2,
+    iAnalytic3,
+    iAnalytic4,
+    iAnalytic5
+
+}
